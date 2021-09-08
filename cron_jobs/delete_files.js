95841931ef069058be1db029f4992f0f801b7ecb
Victor Ahuwanya <viirockn7@gmail.com>
2021-09-07 23:21:20 -0500

    Fixed bugs

--- cron_jobs/delete_files.js
+++ cron_jobs/delete_files.js
@@ -13,15 +13,27 @@ async function run() {
   running = true;
   //Check the database for files that are deleted but are on this server
   //Get 100 of them
-  const files = await File.find({deleted: true, "deleted_on.name": {"$ne": os.hostname()}}).limit(100);
+  const files = await File.find({deleted: true, "location.name": os.hostname(), "deleted_on.name": {"$ne": os.hostname()}}).limit(100);
   //When gotten, delete the file and if successful, update the database that we have deleted it
   for (const file of files) {
-    await promisified_fs_unlink(`${process.env.FILE_STORAGE_LOCATION}${file.filename}`)
-      .then(function () {
-        File.findOneAndUpdate({filename: file.filename}, {"$push": {deleted_on: {name: os.hostname()}}})
-      })
-      .catch(function(err)
-      {})
+    //Check if the file exists
+    //If so, delete
+    //If not, push to database that it has been deleted
+    fs.access(`${process.env.FILE_STORAGE_LOCATION}${file.filename}`, async (err) => {
+      if (err)
+      {
+        await File.findOneAndUpdate({filename: file.filename}, {"$push": {deleted_on: {name: os.hostname()}}});
+      }
+      else{
+        await promisified_fs_unlink(`${process.env.FILE_STORAGE_LOCATION}${file.filename}`)
+          .then(async function () {
+            await File.findOneAndUpdate({filename: file.filename}, {"$push": {deleted_on: {name: os.hostname()}}})
+          })
+          .catch(function(err)
+          {})
+      }
+    })
+
   }
   running = false;
 }
--- cron_jobs/remove_deleted_files.js
+++ cron_jobs/remove_deleted_files.js
@@ -8,8 +8,32 @@ async function run() {
   if (running)
     return;
   running = true;
-  //Delete all files entries where the number of deleted_on elements equals servers count
-  await File.deleteMany({deleted_on: {"$size": secret.servers.length}});
+  //Delete all files entries where the number of deleted_on elements the number of located on elements
+  const files = await File.aggregate([
+    {$match: {deleted: true}},
+    {
+      $addFields: {
+        "a": {$size: "$deleted_on"},
+        "b": {$size: "$location"}
+      }
+    },
+    {
+      $project: {
+        a: 1,
+        b: 1,
+        ab: {$cmp: ["$a", "$b"]}
+      }
+    },
+    {$match: {ab: 0}},
+    {$project: {_id: 1}}
+  ]);
+
+  // console.log(files);
+
+  const ids = files.map(function (file) { return file._id; });
+
+  await File.deleteMany({"_id": {"$in": ids}});
+
   running = false;
 }
 
